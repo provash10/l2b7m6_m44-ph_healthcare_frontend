0e439fea438676934e3fcc479d066617e8a2725c
Provash <provash20cb@gmail.com>
2026-09-26 16:05:00 +0600

    45-1 Setting up React Query Mutation

---
+++ src/api/auth.api.ts
@@ -0,0 +1,8 @@
+import apiClient from "@/lib/apiClient";
+
+export function userLogin(payload : {email:string, password: string}){
+    return apiClient("/auth/login",{
+        method:"POST",
+        body:payload
+    });
+}
